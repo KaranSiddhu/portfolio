19aca793a185a690c2561efd5def3ec77dddefb5
karan siddhu <karansiddhu22@gmail.com>
2022-04-28 21:20:42 +0530

    Completed About Section🚀

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./About.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const abouts = [
+  {
+    title: "Frontend Developer",
+    description: "I am a good frontend Developer.",
+    imgUrl: images.about01
+  },
+  {
+    title: "Backend Developer",
+    description: "I am a good backend Developer.",
+    imgUrl: images.about02
+  },
+  {
+    title: "MERN Stack Developer",
+    description: "I am a good MERN Stack Developer.",
+    imgUrl: images.about02
+  },
+  {
+    title: "App Developer",
+    description: "I am a good App Developer.",
+    imgUrl: images.about03
+  }
+];
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Design</span> <br /> means <span>Good Business</span>
+      </h2>
 
-export default About
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            key={about.title + index}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+          >
+            <img src={about.imgUrl} alt={about.title} />
+
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
