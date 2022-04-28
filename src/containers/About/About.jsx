640c18388b2d8f105b121f77479ad2a3a86fdbf9
karan siddhu <karansiddhu22@gmail.com>
2022-04-28 23:03:04 +0530

    Updated About section

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that <span>Good Design</span> <br /> means <span>Good Business</span>
+        I know that <span>Good Development</span> <br /> means <span>Good Business</span>
       </h2>
 
       <div className="app__profiles">
