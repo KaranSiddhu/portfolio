5cc7848afbd16556e173c167b4d6b76974b12862
KaranSiddhu <karansiddhu22@gmail.com>
2023-11-06 11:51:19 +0530

    Add: fix date year

--- src/Wrapper/AppWrap.js
+++ src/Wrapper/AppWrap.js
@@ -10,7 +10,7 @@ const AppWrap = (Component, idName, classNames) =>
           <Component />
 
           <div className="copyright">
-            <p className="p-text">@2022 Karan</p>
+            <p className="p-text">@{new Date().getFullYear()} Karan</p>
             <p className="p-text">All rights reserved</p>
           </div>
         </div>
